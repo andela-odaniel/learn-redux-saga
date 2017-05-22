308ec45faaa228909be14820aafcf0fd4bacb9af
Daniel Onodje <danielonodje@gmail.com>
2017-05-22 07:47:18 +0100

    refactor reducer

--- reducers.js
+++ reducers.js
@@ -1,12 +1,8 @@
-export default function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'INCREMENT_IF_ODD':
-      return (state % 2 !== 0) ? state + 1 : state
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
+const actions = {
+  'INCREMENT': (action, state = 0) => state + 1,
+  'DECREMENT': (action, state = 0) => state - 1
+}
+
+export default (state = 0, action) => {
+  return actions[action.type] ? actions[action.type](action) : state;
 }
